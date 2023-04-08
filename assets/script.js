6b3910733ca7bfa8dacf25939bf5cb5b3965326d
Brent Houston <brentjustinhouston@gmail.com>
2023-04-08 15:42:40 -0700

    connected api, more styling

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,11 @@
 console.log("hello")
+
+var data = fetch("https://api.openweathermap.org/data/2.5/forecast?lat=47.252876&lon=-122.444290&appid=efa45d9e21f5a963fd87caecdd3b2b02").then(function(res){
+ return res.json(data)
+}).then(function(data)){
+
+    console.log(data)
+};
+
+  
+})
